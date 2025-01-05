21988bd59beaf1d632b6212e784c51e0d99b9747
Vinh nhon Le (U2475293) <U2475293@unimail.hud.ac.uk>
2025-01-05 02:57:20 +0000

    init commit

---
+++ src/test/java/com/example/demo_cd/DemoCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
